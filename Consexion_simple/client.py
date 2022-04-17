59e9e9065b40cb60c353e54d8daa1830fc1122fc
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-04-17 14:37:00 +0200

    programa hecho

--- Consexion_simple/client.py
+++ Consexion_simple/client.py
@@ -3,11 +3,28 @@
 import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+PORT = 65432  # Elpuerto utilizado por elservidor
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+    client_socket.connect((HOST, PORT))
+    message = input(" Escribe la temperatura mínima: ")  # introducir los datos por pantalla
+    client_socket.send(message.encode())  # mandar el mensaje
+    data_temp_min = client_socket.recv(1024).decode()  # recibidos losdatos
 
-print(f"Received {data!r}")
+    message = input(" Escribe la temperatura máxima: ")
+    client_socket.send(message.encode())
+    data_temp_max = client_socket.recv(1024).decode()
+
+    message = input(" Escribe la presión: ")
+    client_socket.send(message.encode())
+    data_presión = client_socket.recv(1024).decode()
+
+    message = input(" Escribe la pluviometría: ")
+    client_socket.send(message.encode())
+    data_pluviometría = client_socket.recv(1024).decode()
+
+print('Recibido del servidor:')
+print(f"Temperatura mínima: {data_temp_min!r}")
+print(f"Temperatura máxima: {data_temp_max!r}")
+print(f"Presión: {data_presión!r}")
+print(f"Pluviometría: {data_presión!r}")
